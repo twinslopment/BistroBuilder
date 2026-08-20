8e94f6fded59027b77af0015dbd221f14174ff05
twinslopment <twinslopment@gmail.com>
2026-08-20 17:31:34 +0200

    4G include transactional restore eligibility gate

--- Assets/Editor/BistroBuilder/Staff/BistroBuilderStaffBlock4ReadinessSelfTest.cs
+++ Assets/Editor/BistroBuilder/Staff/BistroBuilderStaffBlock4ReadinessSelfTest.cs
@@ -64,6 +64,16 @@ public static class BistroBuilderStaffBlock4ReadinessSelfTest
             ref failed);
 
         RunGate(
+            "4D restore elegibilidad transaccional",
+            () => BistroBuilderStaff4DRestoreEligibilitySelfTest.Run(
+                out _,
+                out _,
+                out _),
+            lines,
+            ref passed,
+            ref failed);
+
+        RunGate(
             "4E JSON round-trip",
             () => BistroBuilderStaff4EJsonRoundTripSelfTest.Run(
                 out _,
