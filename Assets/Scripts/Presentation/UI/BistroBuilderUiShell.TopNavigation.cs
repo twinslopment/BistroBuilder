af6b71bbb7d192bfebbc589b879326a4222395b4
twinslopmentGit <twinslopment@gmail.com>
2026-09-23 15:02:48 +0200

    UI: add subtle top navigation hover motion

--- Assets/Scripts/Presentation/UI/BistroBuilderUiShell.TopNavigation.cs
+++ Assets/Scripts/Presentation/UI/BistroBuilderUiShell.TopNavigation.cs
@@ -140,17 +140,26 @@ public sealed partial class BistroBuilderUiShell
         labelRect.anchorMin = new Vector2(0, 0); labelRect.anchorMax = new Vector2(1, 0);
         labelRect.pivot = new Vector2(0.5f, 0);
         labelRect.offsetMin = new Vector2(0, 8); labelRect.offsetMax = new Vector2(0, 27);
+        var glow = HeaderImage(button.transform, "HoverGlow");
+        glow.preserveAspect = false;
+        glow.rectTransform.anchorMin = Vector2.zero; glow.rectTransform.anchorMax = Vector2.one;
+        glow.rectTransform.offsetMin = new Vector2(2, 2); glow.rectTransform.offsetMax = new Vector2(-2, -2);
+        glow.color = new Color(BBIconDesignTokens.SelectedSoft.r, BBIconDesignTokens.SelectedSoft.g, BBIconDesignTokens.SelectedSoft.b, 0f);
+        glow.transform.SetAsFirstSibling();
+
         var icon = HeaderImage(button.transform, "NavigationIcon");
         icon.rectTransform.anchorMin = icon.rectTransform.anchorMax = new Vector2(0.5f, 1);
         icon.rectTransform.pivot = new Vector2(0.5f, 1);
         icon.rectTransform.sizeDelta = new Vector2(23, 23); icon.rectTransform.anchoredPosition = new Vector2(0, -9);
+        icon.rectTransform.localScale = Vector3.one;
+        icon.rectTransform.localRotation = Quaternion.identity;
         var underline = HeaderImage(button.transform, "SelectionUnderline");
         underline.rectTransform.anchorMin = Vector2.zero; underline.rectTransform.anchorMax = new Vector2(1, 0);
         underline.rectTransform.offsetMin = new Vector2(7, 0); underline.rectTransform.offsetMax = new Vector2(-7, 3);
         var fx = button.GetComponent<BBIconButton>();
         if (fx == null) fx = button.gameObject.AddComponent<BBIconButton>();
         fx.ConfigureRuntime(id, icon, button);
-        fx.ConfigureNavigationSurface(button.GetComponent<Image>(), underline, label);
+        fx.ConfigureNavigationSurface(button.GetComponent<Image>(), underline, label, glow);
         fx.SetToggleSelectionOnClick(false);
         return button;
     }
