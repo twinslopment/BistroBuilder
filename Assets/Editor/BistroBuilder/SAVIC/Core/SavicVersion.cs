a1f8dedebf673a13f1e18958785698824b31296a
twinslopment <twinslopment@gmail.com>
2026-09-25 20:22:29 +0200

    feat(savic): bump queue schema for recovery metadata

--- Assets/Editor/BistroBuilder/SAVIC/Core/SavicVersion.cs
+++ Assets/Editor/BistroBuilder/SAVIC/Core/SavicVersion.cs
@@ -4,7 +4,7 @@ namespace BistroBuilder.Editor.Savic
     {
         internal const string ProductVersion = "0.1.0";
         internal const int ManifestSchemaVersion = 1;
-        internal const int QueueSchemaVersion = 1;
+        internal const int QueueSchemaVersion = 2;
         internal const string PipelineVersion = "intake-v1";
     }
 }
