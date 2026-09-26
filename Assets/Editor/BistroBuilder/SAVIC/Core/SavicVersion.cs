2e24025c872564b93f47cb333f5b66eb11fbfcd5
twinslopment <twinslopment@gmail.com>
2026-09-26 13:17:57 +0200

    refactor(savic): bump queue schema for staged source pipeline

--- Assets/Editor/BistroBuilder/SAVIC/Core/SavicVersion.cs
+++ Assets/Editor/BistroBuilder/SAVIC/Core/SavicVersion.cs
@@ -4,7 +4,7 @@ namespace BistroBuilder.Editor.Savic
     {
         internal const string ProductVersion = "0.1.0";
         internal const int ManifestSchemaVersion = 1;
-        internal const int QueueSchemaVersion = 4;
+        internal const int QueueSchemaVersion = 5;
         internal const string PipelineVersion = "intake-v1";
     }
 }
