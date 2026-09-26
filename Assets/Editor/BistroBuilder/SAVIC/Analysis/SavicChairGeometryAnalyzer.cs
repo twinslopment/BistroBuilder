daf7329f69af825714050eb6d96743611ffdbccb
twinslopment <twinslopment@gmail.com>
2026-09-26 18:51:42 +0200

    refactor(savic): invalidate pre-metric-space analysis caches

--- Assets/Editor/BistroBuilder/SAVIC/Analysis/SavicChairGeometryAnalyzer.cs
+++ Assets/Editor/BistroBuilder/SAVIC/Analysis/SavicChairGeometryAnalyzer.cs
@@ -9,7 +9,7 @@ namespace BistroBuilder.Editor.Savic
 {
     internal static class SavicChairGeometryAnalyzer
     {
-        internal const string Version = "1.0.0";
+        internal const string Version = "2.0.0";
 
         private const int VerticalBinCount = 24;
         private const int MaximumSampledTrianglesPerMeshInstance = 200000;
